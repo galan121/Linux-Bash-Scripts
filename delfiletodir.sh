3a6ddd1923f62897db4dd01ddba964bacee5177d
galan121 <114428661+galan121@users.noreply.github.com>
2023-06-15 15:41:37 +0300

    Update delfiletodir.sh

--- delfiletodir.sh
+++ delfiletodir.sh
@@ -3,9 +3,9 @@ read -p “Введите путь к директории”
 for directory  in ~/*
    do
 if [ -d “$directory” ] 
-    rm –v  *.bak   *.tmp   *.backup 
+    rm –v  *.bak *.tmp *.backup 
     echo “ Заданные файлы удалены из репозитория”
 else:
     echo “Директория не найдена”
 fi
-done
+
