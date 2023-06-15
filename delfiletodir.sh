db1ba906c4a658e953e8402492fb8d15ac5c0a02
galan121 <114428661+galan121@users.noreply.github.com>
2023-06-15 15:24:17 +0300

    Update delfiletodir.sh

--- delfiletodir.sh
+++ delfiletodir.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
-
-read -p "Введите путь к дирректории: " DELDIR
-
-if [ -e $DELDIR ]
-        then
-                echo 'Указанная дирректория найдена'
-                cd $DELDIR
-                echo 'Произвожу удаление'
-                rm -v *.bak *.tmp *.backup
-                echo 'Удаление завершено успешно'
-        else
-                echo 'Указанная дирректория не найдена! Выполнение остановлено'
-                exit 2
+#!/usr/bin/python3
+read -p “Введите путь к директории”
+for directory  in ~/*
+do
+if [  -d “$directory” ] 
+   rm –v  *.bak   *.tmp   *.backup 
+echo “ Заданные файлы удалены из репозитория”
+else:
+echo “Директория не найдена”
 fi
