3a167ba17d5a2640a2c31c73a5e26978ef0ef67b
galan121 <114428661+galan121@users.noreply.github.com>
2023-06-20 11:19:07 +0300

    Update directory cleanup script.sh

--- directory cleanup script.sh
+++ directory cleanup script.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-read -p “Введите путь к директории”
-for file in ~/*
-   do
-if [ -d “$file” ] 
-    then 
-         echo "Директория найдена , произвожу удаление"
-         rm –v *.bak *.tmp *.backup 
-         echo "Заданные файлы удалены"
-     else
-         echo "Директория не найдена"
-       
- fi
-done
-  
-
+FILE=/home/andrey/DZ_4
+if [ -e "$FILE" ]; 
+   then
+       echo "Директория найдена , произвожу удаление"
+       rm –v *.bak *.tmp *.backup 
+       echo "Заданные файлы удалены"
+    else
+       echo "Директория не найдена"
+    exit   
+fi
